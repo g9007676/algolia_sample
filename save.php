74094ad562871569116c68d6558ea5a343c7f8a1
哲宇 林 <James@zheyude-MacBook-Pro.local>
2017-06-11 14:50:28 +0800

    save api

---
+++ save.php
@@ -0,0 +1,12 @@
+<?php
+require(__DIR__ . '/vendor/autoload.php');
+$client = new \AlgoliaSearch\Client("WKDDRK70D7", "71d5ed293564487794308eb423204d14");
+
+$index = $client->initIndex('test');
+
+$index->saveObject(
+    [
+        'objectID' => '201705290000000002',
+        'name'  => 'CHANEL 香奈兒 山茶花保濕'
+    ]
+);
